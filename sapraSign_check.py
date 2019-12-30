c16cd2ba3b7ec0a74799941865241b0f391b3b10
ar.gilaki <ar.gilaki@gmail.com>
2019-12-30 14:10:12 +0330

    wget for requesting page was used

--- sapraSign_check.py
+++ sapraSign_check.py
@@ -1,53 +1,69 @@
 import pandas as pd
-import requests
-from bs4 import BeautifulSoup
+# import requests
+# from bs4 import BeautifulSoup
 import webbrowser as wb
-import datetime.datetime as DATE
+from datetime import datetime as DATE
+import os
 
+# os.system('wget http://hjakgjkdf.fggf --no-check-certificate')
+# print('############################################')
 wCtlr= wb.get('firefox')
 
 
-# try:
-#
-#     result= requests.get('https://www.uptvs.com/' , verify=False)
-#     page= result.text
-#     print('###########################################')
-#
-# except Exception:
-#     print(11111111111111111111111111)
-#     wCtlr.open('https://www.uptvs.com/')
 
 def search_in_site (site,keyword):
-    result = requests.get(site , verify=False)
-    page = result.text
-    print(site)
-    soup = BeautifulSoup(page, 'html.parser')
-    s= page.find(keyword)
-    return s
+    with open(site+'.txt', 'r') as file:
+        data = file.read()
+    s= data.find(keyword)
+    if data == '':
+        out= 'service unavailable'
+    else:
+        if s != -1:
+            out= 'دارای نماد ساپرا'
+
+        else:
+            out= 'فاقد نماد ساپرا'
+
+    return out
 
 
 df = pd.read_excel('sitesList.xlsx', index_col=0)
 sitesList = list(df.index)
 check=[]
 for site in sitesList:
-    try:
-        s= search_in_site('https://'+site,"offers.sapra.ir")
-        print(s)
-        if s != -1:
-            check.append([1])
-        else:
-            check.append([0])
-    except Exception:
-        try:
-            s= search_in_site('http://'+site,"offers.sapra.ir")
-            if s != -1:
-                check.append([1])
-            else:
-                check.append([0])
-        except Exception:
-                check.append(["service unavailable"])
-                wCtlr.open('https://'+site)  #just for checking weather it is unavailable or not by open the site in firefox browser
-x= DATE()
+    site= site.lower()
+    os.system('wget -O ./'+site+ '.txt https://'+site+' --tries=1 --no-check-certificate')
+    out= search_in_site(site,"offers.sapra.ir")
+    if out != 'service unavailable':
+        check.append([out])
+        continue
+    os.system('wget -O ./'+site+ '.txt http://'+site+' --tries=1 --no-check-certificate')
+    out = search_in_site(site,"offers.sapra.ir")
+    check.append([out])
+    if out == 'service unavailable':
+         wCtlr.open('https://'+site)
+
+
+    # try:
+    #     os.system('wget -O ./'+site+ '.txt https://'+site+' --no-check-certificate')
+    #     s= search_in_site(site,"offers.sapra.ir")
+    #     if s != -1:
+    #         check.append(["دارای نماد ساپرا"])
+    #     else:
+    #         check.append(["فاقد نماد ساپرا"])
+    # except Exception:
+    #     try:
+    #         os.system('wget -O ./'+site+ '.txt http://'+site+' --no-check-certificate')
+    #         s= search_in_site(site,"offers.sapra.ir")
+    #         if s != -1:
+    #             check.append(["دارای نماد ساپرا"])
+    #         else:
+    #             check.append(["فاقد نماد ساپرا"])
+    #     except Exception:
+    #             check.append(["service unavailable"])
+    #             wCtlr.open('https://'+site)  #just for checking weather it is unavailable or not by open the site in firefox browser
+
+x= DATE.now()
 xname=  x.strftime('%d')+x.strftime('%b')+x.strftime('%Y')
 xname= str(xname)+'.xlsx'
 df1 = pd.DataFrame(check,index= df.index,columns=df.columns)
